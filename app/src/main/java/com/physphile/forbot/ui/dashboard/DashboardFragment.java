803b96ccb33a8a6910dddb8a595e673f2bf5a638
Artem <netsvetaev.mgn@gmail.com>
2020-02-21 17:46:15 +0500

    Опять дебажить))

--- app/src/main/java/com/physphile/forbot/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/physphile/forbot/ui/dashboard/DashboardFragment.java
@@ -49,38 +49,9 @@ public class DashboardFragment extends Fragment {
         database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference();
 
-        cnt = new HashMap<>();
+//        cnt = new HashMap<>();
         adapter = new OlympAdapter(getContext(), R.layout.olympitem);
         OlympsList.setAdapter(adapter);
-
-        // Пример добавления элементов
-
-        /*
-        Item it1 = new Item("kek1", "1");
-        Item it2 = new Item("kek2", "2");
-        Item it3 = new Item("kek3", "3");
-        Item it4 = new Item("kek4", "1");
-        Item it5 = new Item("kek5", "2");
-        Item it6 = new Item("kek6", "3");
-        Item it7 = new Item("kek7", "3");
-        Item it8 = new Item("kek8", "3");
-        Item it9 = new Item("kek9", "3");
-
-        addItemToDay(2020, 1, 22, it1);
-        addItemToDay(2020, 1, 22, it2);
-        addItemToDay(2020, 1, 22, it3);
-        addItemToDay(2020, 1, 21, it4);
-        */
-
-//        databaseReference.child("SomDir").push().setValue(it1);
-//        databaseReference.child("SomDir").push().setValue(it2);
-//        databaseReference.child("SomDir").push().setValue(it3);
-//        databaseReference.child("SomDir").push().setValue(it4);
-//        databaseReference.child("SomDir").push().setValue(it5);
-//        databaseReference.child("SomDir").push().setValue(it6);
-//        databaseReference.child("SomDir").push().setValue(it7);
-//        databaseReference.child("SomDir").push().setValue(it8);
-//        databaseReference.child("SomDir").push().setValue(it9);
 
         Date tmp = new Date(calendarView.getDate());
         setItemByDate(tmp.getYear(), tmp.getMonth(), tmp.getDay());
@@ -90,67 +61,57 @@ public class DashboardFragment extends Fragment {
     }
 
     private String makePath (int year, int month, int dayOfMonth) {
-        String ans = Integer.toString(year);
-        ans += '/';
-        ans += Integer.toString(month);
-        ans += '/';
-        ans += Integer.toString(dayOfMonth);
-        return ans;
+        return year + "/" + month + "/" + dayOfMonth;
     }
 
-    private int countChildrenOnDirectory (DatabaseReference ref) {
-        int hash = ref.hashCode();
-        int ans = sp.getInt(""+hash, -1);
-        if (ans == -1) return 0;
-        else return ans;
-    }
+//    private int countChildrenOnDirectory (DatabaseReference ref) {
+//        int hash = ref.hashCode();
+//        int ans = sp.getInt(""+hash, -1);
+//        if (ans == -1) return 0;
+//        else return ans;
+//    }
 
-    private void incrementChildrenOnDirectory (DatabaseReference ref) {
-        int hash = ref.hashCode();
-        int t = sp.getInt(""+hash, -1);
-        SharedPreferences.Editor e = sp.edit();
-        if (t == -1) e.putInt(""+hash, 1);
-        else e.putInt(""+hash, t + 1);
-        e.apply();
-    }
+//    private void incrementChildrenOnDirectory (DatabaseReference ref) {
+//        int hash = ref.hashCode();
+//        int t = sp.getInt(""+hash, -1);
+//        SharedPreferences.Editor e = sp.edit();
+//        if (t == -1) e.putInt(""+hash, 1);
+//        else e.putInt(""+hash, t + 1);
+//        e.apply();
+//    }
 
-    private void addItemToDay (int year, int month, int dayOfMonth, Item item) {
-        String curDate = makePath(year, month, dayOfMonth);
-        DatabaseReference newRef = databaseReference.child(curDate);
-        int c = countChildrenOnDirectory(newRef);
-        newRef = newRef.child("" + c);
-        newRef.setValue(item);
-        incrementChildrenOnDirectory(databaseReference.child(curDate));
-    }
+//    private void addItemToDay (int year, int month, int dayOfMonth, Item item) {
+//        String curDate = makePath(year, month, dayOfMonth);
+//        DatabaseReference newRef = databaseReference.child(curDate);
+//        int c = countChildrenOnDirectory(newRef);
+//        newRef = newRef.child("" + c);
+//        newRef.setValue(item);
+//        incrementChildrenOnDirectory(databaseReference.child(curDate));
+//    }
 
     private void setItemByDate (int year, int month, int dayOfMonth) {
         adapter.clear();
         String curDate = makePath(year, month, dayOfMonth);
-        DatabaseReference newRef = databaseReference.child(curDate);
-        int c = countChildrenOnDirectory(newRef);
-        if (c == 0) {
-            Toast.makeText(getContext(), "На этот день нет олимпиад", Toast.LENGTH_SHORT).show();
-        } else {
-            for (int i = 0; i < c; ++i) {
-                DatabaseReference tmpRef = newRef.child(""+i);
-                tmpRef.addChildEventListener(new ChildEventListener() {
-                    @Override
-                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                        Item item = dataSnapshot.getValue(Item.class);
-                        adapter.add(item);
-                    }
+        DatabaseReference newRef = database.getReference(curDate);
+//        int c = countChildrenOnDirectory(newRef);
+//        for (int i = 0; i < c; ++i) {
+//            DatabaseReference tmpRef = newRef.child(""+i);
+            newRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    Item item = dataSnapshot.getValue(Item.class);
+                    adapter.add(item);
+                }
 
-                    @Override
-                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
-                    @Override
-                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
-                    @Override
-                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {}
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {}
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {}
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s){}
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {}
                 });
-            }
-        }
     }
 
     private CalendarView.OnDateChangeListener onDateChangeListener= new CalendarView.OnDateChangeListener() {
